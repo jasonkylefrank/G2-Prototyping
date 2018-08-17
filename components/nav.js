fb9f27384fc74004c1332d091475522791737c48
Jason K. Frank <jasonkylefrank@gmail.com>
2018-08-17 17:11:19 -0400

    Added prefetching & Nav copmonent refactoring

--- components/nav.js
+++ components/nav.js
@@ -1,36 +1,53 @@
 import Link from 'next/link';
-//import { assetPrefix } from '../next.config';
+import { siteURLs } from '../site-config/site-urls';
 
-export default (props) => {
+export default (props) => {    
 
-    const urls = {
-        'home': { 'label': 'Home', 'route': '/' },
-        'about': { 'label': 'About', 'route': '/about' },
-        'schedule': { 'label': 'Schedule', 'route': '/schedule' }
+    const renderLinkAnchor = (linkText) => {
+        return <a>{linkText}</a>;
     };
 
-    const renderItem = (linkText, url, index) => {
-        // The assetPrefix solves the Github project pages URL prefix problem
+    const renderPrefetchLink = (linkText, url) => {
+        return (
+            <Link href={url} prefetch>
+                {renderLinkAnchor(linkText)}
+            </Link>
+        )
+    };
+
+    const renderNonPrefetchLink = (linkText, url) => {
+        return (
+            <Link href={url}>
+                {renderLinkAnchor(linkText)}
+            </Link>
+        )
+    };
+
+    const renderItem = (linkText, url, shouldPrefetch, index) => {
+        const link = (shouldPrefetch
+            ? renderPrefetchLink(linkText, url)
+            : renderNonPrefetchLink(linkText, url)
+        );
+
         return (
             <li key={index}>
-                {/* <Link href={url} as={assetPrefix + url}> */}
-                <Link href={url}>
-                    <a>{linkText}</a>
-                </Link>
+                {link}
             </li>
         );
     };
 
     const renderItems = () => {
-        const urlKeys = Object.keys(urls);
+        const urlKeys = Object.keys(siteURLs);
         return urlKeys.map((urlKey, i) => (
-            renderItem(urls[urlKey].label, urls[urlKey].route, i)
+            renderItem(siteURLs[urlKey].label, siteURLs[urlKey].route, siteURLs[urlKey].prefetch, i)
         ));
     };
 
     return (
-        <ul>
-            {renderItems()}
-        </ul>
+        <nav>
+            <ul>
+                {renderItems()}
+            </ul>
+        </nav>
     );
 };
