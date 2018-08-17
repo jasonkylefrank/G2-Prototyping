5ec794a76866511c4cbd0dbc7a3abfc0695fad48
Jason K. Frank <jasonkylefrank@gmail.com>
2018-08-17 15:19:27 -0400

    Added text to test Netlify branch deployment.

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default (props) => (
         <div className="page">
             <h4>Welcome to the G2 PM prototyping app</h4>
             <p>More welcome stuff coming soon.  For now, try navigating to "/schedule" or "/about"</p>
+            <p>Does this work on Netlify?</p>
         </div>
     </div>
 );
